8a5c9f0d6421fd55c51579a632c26e3761a44769
Kimeshan Naidoo <contact@kimeshan.com>
2015-03-20 15:05:29 +0200

    Inital commit: Maxflow Angular/MEAN App

---
+++ public/js/controllers/NerdCtrl.js
@@ -0,0 +1,7 @@
+// public/js/controllers/NerdCtrl.js
+var nerdApp = angular.module('NerdCtrl', []);
+nerdApp.controller('NerdController', function($scope) {
+
+    $scope.tagline = 'Nothing beats a pocket protector!';
+
+});
---
+++ server.js
@@ -0,0 +1,81 @@
+// server.js
+// modules =================================================
+var express        = require('express');
+var app            = express();
+var bodyParser     = require('body-parser');
+var methodOverride = require('method-override');
+var multer     =       require('multer');
+//var ffmpeg     =       require("fluent-ffmpeg");
+
+// configuration ===========================================
+    
+// config files
+//var db = require('./config/db');
+
+// set our port
+var port = process.env.PORT || 3000; 
+
+// connect to our mongoDB database 
+// (uncomment after you enter in your own credentials in config/db.js)
+// mongoose.connect(db.url); 
+
+// middleware ==============================================
+// get all data/stuff of the body (POST) parameters
+// parse application/json 
+//app.use(bodyParser.json()); 
+
+// parse application/vnd.api+json as json
+//app.use(bodyParser.json({ type: 'application/vnd.api+json' })); 
+
+// parse application/x-www-form-urlencoded
+//app.use(bodyParser.urlencoded({ extended: true })); 
+
+// override with the X-HTTP-Method-Override header in the request. simulate DELETE/PUT
+//app.use(methodOverride('X-HTTP-Method-Override')); 
+
+//multer for file uploading
+
+
+var newFileName;
+
+//Parse the file name before uploading file
+app.use('/fileName', bodyParser.json(),function (request,response,next) {
+	newFileName = request.body.file_name;
+	response.send("Done parsing file name.");
+});
+
+app.use('/upload',multer({ dest: './Video Uploads/',
+
+rename: function (fieldname,filename,request) {
+    return newFileName;
+ },
+
+onFileUploadStart: function (file) {
+  console.log(file.originalname + ' is starting ...');
+},
+
+onFileUploadComplete: function (file,request,response) {
+  console.log(file.originalname + ' uploaded to  ' + file.path);
+  fileName = file.originalname;
+  uploadPath = file.path;
+  response.send([fileName,uploadPath]);
+  //startVideoConversion(fileName,uploadPath);
+}
+}));
+
+
+// set the static files location /public/img will be /img for users
+app.use(express.static(__dirname + '/public')); 
+
+// routes ==================================================
+require('./app/routes')(app); // configure our routes
+
+// start app ===============================================
+// startup our app at http://localhost:3000
+app.listen(port);               
+
+// shoutout to the user                     
+console.log('Magic happens on port ' + port);
+
+// expose app           
+exports = module.exports = app;     
